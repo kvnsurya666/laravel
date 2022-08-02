47503a9944025ad820986488999135939cccbbc3
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-08-02 17:17:49 +0700

    edit promo

--- resources/views/promo.blade.php
+++ resources/views/promo.blade.php
@@ -8,4 +8,45 @@
             </div>
         </div>
     </div>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <h2>Promo Bulan ini</h2>
+        </div>    
+      </div>
+      <div class="row" style="padding-bottom: 10px;">
+          <div class="card col-md-3" style="width: 20rem; padding: 10px">
+              <img src="/assets/img/Logo/weida.png" class="card-img-top" alt="WEIDA" />       
+          </div>
+
+          <div class="card col-md-3" style="width: 20rem; padding: 10px">
+              <img src="/assets/img/Logo/dixon.jpg" class="card-img-top" alt="DIXON" />
+          </div>
+
+          <div class="card col-md-3" style="width: 20rem; padding: 10px">
+              <img src="/assets/img/Logo/fullmark.jpg" class="card-img-top" alt="FULLMARK" />
+          </div>
+
+          <div class="card col-md-3" style="width: 20rem; padding: 10px">
+              <img src="/assets/img/Logo/bositools.png" class="card-img-top" alt="BOSI TOOLS" />
+          </div>
+      </div>
+      <div class="row" style="padding-bottom: 10px;">
+          <div class="card col-md-3" style="width: 20rem; padding: 10px">
+              <img src="/assets/img/Logo/J&W.jpg" class="card-img-top" alt="J&W" />       
+          </div>
+
+          <div class="card col-md-3" style="width: 20rem; padding: 10px">
+              <img src="/assets/img/Logo/Mitutoyo.png" class="card-img-top" alt="Mitutoyo" />
+          </div>
+
+          <div class="card col-md-3" style="width: 20rem; padding: 10px">
+              <img src="/assets/img/Logo/NACHI.jpg" class="card-img-top" alt="NACHI" />
+          </div>
+
+          <div class="card col-md-3" style="width: 20rem; padding: 10px">
+              <img src="/assets/img/Logo/fullmark.jpg" class="card-img-top" alt="Fullmark" />
+          </div>
+      </div>
+    </div>
 @endsection
