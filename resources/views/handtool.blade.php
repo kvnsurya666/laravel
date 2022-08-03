2a6f2f28fa457e85e18b3475df178022f56e45ad
Fransiscus Kevin <kevinsuryaperdana@gmail.com>
2022-08-03 10:58:16 +0700

    banner testing

--- resources/views/handtool.blade.php
+++ resources/views/handtool.blade.php
@@ -1,1 +1,13 @@
 @extends('CJL.layouts.template')
+
+@section('content')
+    <div class="container-fluid">
+      <div id="carouselSlide" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="/img/Banner/handtools.jpg" class="d-block w-100" alt="Gambar 1" />
+          </div>
+        </div>
+      </div>
+    </div>
+@endsection
